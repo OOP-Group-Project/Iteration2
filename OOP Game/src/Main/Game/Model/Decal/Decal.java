100b1af2dc141d4545f709666ed498c63dc94c22
walkharddotninja <smith.phillip.m@live.com>
2016-03-08 20:51:35 -0500

    Worked on Decal

---
+++ OOP Game/src/Main/Game/Model/Decal/Decal.java
@@ -0,0 +1,13 @@
+package Main.Game.Model.Decal;
+
+/**
+ * Created by walkhard on 3/8/16.
+ */
+public class Decal {
+    /*
+        STAR = Gold Star
+        CROSS = Red Cross
+        SKULL = Skull and Crossbones
+     */
+    public enum Type {STAR, CROSS, SKULL}
+}
