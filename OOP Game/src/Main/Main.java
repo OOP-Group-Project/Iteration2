a1444b993e79bbd3887e96c37f18b710595340a8
AndyZhu <zhuyidiwow@gmail.com>
2016-03-12 00:11:30 -0500

    Forgot to change the Main class back in last commit

--- OOP Game/src/Main/Main.java
+++ OOP Game/src/Main/Main.java
@@ -1,24 +1,8 @@
 package Main;
 
-import Main.Model.Map.MapLocationPoint;
-import Main.Model.Skills.RadialEffects;
-import sun.misc.Queue;
-
 public class Main {
     public static void main(String[] args) {
-        //Game game = new Game();
-        //game.start();
-
-        RadialEffects er = new RadialEffects();
-        Queue<MapLocationPoint> q = er.getAffectedArea(0, 0, 3);
-        Queue<Integer> i = new Queue<>();
-        while (!q.isEmpty()) {
-            try {
-                System.out.println(q.dequeue());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
+        Game game = new Game();
+        game.start();
     }
 }
