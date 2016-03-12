a99c283bc00ac29b586b4ca4979b8bfc6931acb5
Peter Camejo <thealex123@gmail.com>
2016-03-12 18:09:16 -0500

    Further additions to Weapon

--- OOP Game/src/Main/Model/Items/Weapon.java
+++ OOP Game/src/Main/Model/Items/Weapon.java
@@ -1,6 +1,6 @@
 package Main.Model.Items;
 
-
+import Main.Model.Actions.Attack;
 /**
  * Created by Peter Camejo on 3/9/2016.
  * TODO: Update First Constructor when Equippable is implemented.
@@ -10,14 +10,16 @@ public class Weapon extends Equippable {
     private WeaponTypeEnum weaponType;
     private boolean twoHanded;
     private float attackSpeed;
+    private Attack attack;
 
     /*** Constructors ***/
     //Sets twoHanded based on weaponType
-    public Weapon(/* EQuipable Parameters */ WeaponTypeEnum weaponType, float attackSpeed) {
+    public Weapon(/* EQuipable Parameters */ WeaponTypeEnum weaponType, float attackSpeed, Attack attack) {
 
         //super( Equipable parameters);
         this.attackSpeed = attackSpeed;
         this.weaponType = weaponType;
+        this.attack = attack;
 
         if (weaponType == WeaponTypeEnum.ONEHANDSWORD ||
                 weaponType == WeaponTypeEnum.SHIELD ||
@@ -34,6 +36,8 @@ public class Weapon extends Equippable {
     public WeaponTypeEnum getWeaponType(){
         return this.weaponType;
     }
+    public Attack getAttack(){return attack;}
+    public void setAttack(Attack attack){this.attack = attack;}
     public float getAttackSpeed(){ return this.attackSpeed;}
     public void setAttackSpeed(float attackSpeed){this.attackSpeed = attackSpeed;}
     public boolean isTwoHanded(){ return this.twoHanded;}
