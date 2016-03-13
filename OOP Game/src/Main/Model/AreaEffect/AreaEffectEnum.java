665c1f5f26d8802280f1e92e9f8498934f475aa8
Peter Camejo <thealex123@gmail.com>
2016-03-13 17:48:13 -0400

    Added a WeakeningTrap

--- OOP Game/src/Main/Model/AreaEffect/AreaEffectEnum.java
+++ OOP Game/src/Main/Model/AreaEffect/AreaEffectEnum.java
@@ -4,5 +4,5 @@ package Main.Model.AreaEffect;
  * Created by Michael on 3/10/16.
  */
 public enum AreaEffectEnum {
-    HEAL,DEATH,LEVELUP,DAMAGE,PORTAL, TRAP , SPEEDUP;
+    HEAL,DEATH,LEVELUP,DAMAGE,PORTAL, TRAP , SPEEDUP
 }
