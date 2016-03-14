2e47954479082567f6a421a828c3e359dee2ce6c
Mason Turner <masonturner@ufl.edu>
2016-03-14 02:48:06 -0400

    Forgot to merge the files that were in the wrong directories.

--- OOP Game/src/Main/Controller/ObjectControllers/EntityController/ActionControllers/SkillsControllers/SneakController.java
+++ OOP Game/src/Main/Controller/ObjectControllers/EntityController/ActionControllers/SkillsControllers/SneakController.java
@@ -1,10 +1,7 @@
 package Main.Controller.ObjectControllers.EntityController.ActionControllers.SkillsControllers;
 
 import Main.Controller.Manager.UserActionEnum;
-import Main.Model.AreaEffect.AreaEffectEnum;
-import Main.Model.AreaEffect.Traps.Trap;
 import Main.Model.Entity.Entity;
-import Main.Model.Items.WeaponTypeEnum;
 import Main.Model.Map.Map;
 import Main.Model.Map.MapLocationPoint;
 import Main.Model.Skills.*;
@@ -23,55 +20,47 @@ public class SneakController {
     }
 
     public void performSkill(UserActionEnum u) {
-      /*  switch (u) {
+        switch (u) {
             case Skill1:
-                if (new PickPocket(sneak).activate())
-                    pickpocket();
+                if (validatePickPocket()) new PickPocket(sneak).activate();
                 break;
             case Skill2:
-                if (new DetectTrap(sneak).activate()) uncoverTrap();
-                    if (new RemoveTrap(sneak).activate()) disableTrap();
+                //  if (validateDetectTrap()) new DetectTrap().activate();
+                //  if (validateRemoveTrap()) new RemoveTrap().activate();
                 break;
             case Skill3:
-                Creep c = new Creep(sneak);
-                //TODO: modifyStats of enemies instead
-                if (c.activate())
-                    sneak.getStats().modifyStats(c.attack());
+                // if (validateCreep()) new Creep().activate();
                 break;
             case Skill4:
-                if (validateRanged())
-                    sneak.getStats().modifyStats(new RangedWeapon(sneak).attack());
+                if (validateRangedWeapon()) new RangedWeapon(sneak).activate();
                 break;
             default:
                 System.out.print("Something went wrong in" + this.toString());
-        }*/
-    }
-
-    private void disableTrap() {
-        MapLocationPoint targetTile = sneak.getLocation().getAdjacent(sneak.getOrientation());
-        map.getTile(targetTile.x,targetTile.y).removeAreaEffect();
-    }
-
-    //checks to see if there is a tile with a trap in front of the entity and if so uncover it
-    private void uncoverTrap() {
-        MapLocationPoint targetTile = sneak.getLocation().getAdjacent(sneak.getOrientation());
-        Trap ae = (Trap)map.getTile(targetTile.x, targetTile.y).getAreaEffect();
-        if (ae.getType() == AreaEffectEnum.TRAP)
-            ae.setIsVisible(true);
-    }
-
-    //checks to see if there is an entity in front of the current entity and if so add the first item in their inventory to its own
-    private void pickpocket() {
-        Entity target = map.getTile(sneak.getLocation().x, sneak.getLocation().y).getEntity();
-        if (target != null && target.getInventory().isThereAnItemAt(0))
-            sneak.getInventory().addItem(target.getInventory().getItemAt(0));
-    }
-
-    private boolean validateRanged() {
-        if (sneak.getEquipment().getWeapon().getWeaponType() == WeaponTypeEnum.FIST)
-            return true;
-        else {
-            return false;
         }
+    }
+
+    private boolean validateRangedWeapon() {
+        return false;
+    }
+
+    private boolean validateCreep() {
+        return false;
+    }
+
+    private boolean validateRemoveTrap() {
+        return false;
+    }
+
+    private boolean validateDetectTrap() {
+        MapLocationPoint point = sneak.getLocation().getAdjacent(sneak.getOrientation());
+        if (true) return true;
+        return false;
+    }
+
+    //check to see if theres an entity if so apply pickpocket
+    private boolean validatePickPocket() {
+        MapLocationPoint point = sneak.getLocation().getAdjacent(sneak.getOrientation());
+//        if (map.getTile(point.x, point.y).getEntity()) return true;
+        return true;
     }
 }
--- OOP Game/src/Main/Controller/ObjectControllers/EntityController/ActionControllers/SkillsControllers/SummonerController.java
+++ OOP Game/src/Main/Controller/ObjectControllers/EntityController/ActionControllers/SkillsControllers/SummonerController.java
@@ -3,7 +3,6 @@ package Main.Controller.ObjectControllers.EntityController.ActionControllers.Ski
 import Main.Controller.Manager.UserActionEnum;
 import Main.Model.Entity.Entity;
 import Main.Model.Map.Map;
-import Main.Model.Map.MapLocationPoint;
 import Main.Model.Skills.Bane;
 import Main.Model.Skills.Boon;
 import Main.Model.Skills.Enchantment;
@@ -26,20 +25,13 @@ public class SummonerController {
     public void performSkill(UserActionEnum u) {
         switch (u) {
             case Skill1:
-                Enchantment ec = new Enchantment(summoner);
-                if (ec.allCheck()) applyEnchantment(ec);
+                new Enchantment(summoner).activate();
                 break;
             case Skill2:
-                Boon bn = new Boon(summoner);
-                if (bn.allCheck()) {
-                    summoner.getStats().modifyStats(bn.activate());
-                    double duation = bn.getDuration();
-                }
-
+                new Boon(summoner).activate();
                 break;
             case Skill3:
-                Bane be = new Bane(summoner);
-                if (be.allCheck()) applyBane(be);
+                new Bane(summoner).activate();
                 break;
             case Skill4:
                 new Staff(summoner).activate();
@@ -47,17 +39,5 @@ public class SummonerController {
             default:
                 System.out.print("Something went wrong in" + this.toString());
         }
-    }
-
-    private void applyBane(Bane be) {
-        MapLocationPoint targetPoint = summoner.peek();
-        Entity target = map.getTile(targetPoint.x,targetPoint.y).getEntity();
-        if(target != null) target.getStats().modifyStats(be.activate());
-    }
-
-    private void applyEnchantment(Enchantment ec) {
-        MapLocationPoint point = summoner.peek();
-        Entity target = map.getTile(point.x,point.y).getEntity();
-//        if(target != null) target.getStats().modifyStats(ec.activate());
     }
 }
