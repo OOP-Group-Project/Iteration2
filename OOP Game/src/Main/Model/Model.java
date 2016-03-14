a921809e85a89e99437c64ff4e094350308d8390
Jorge Varela <jcvarela@live.com>
2016-03-13 23:33:55 -0400

    setting up map creation

--- OOP Game/src/Main/Model/Model.java
+++ OOP Game/src/Main/Model/Model.java
@@ -55,7 +55,7 @@ public class Model {
         states.put(StateEnum.StatState, new StatState(player));
 
         states.put(StateEnum.StartMenuState, new StartMenuState());
-        states.put(StateEnum.AvatarCreationState,new AvatarCreationState(player));
+        states.put(StateEnum.AvatarCreationState,new AvatarCreationState(player,world));
 
         new MapIO().saveMap(world, "map.txt");
     }
