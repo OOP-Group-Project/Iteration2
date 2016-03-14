ad8365327ff514a04119712e733edbdadb472c0f
mbentz <mbentz@ufl.edu>
2016-03-14 02:50:13 -0400

    Fixes bug, code now compiles

--- OOP Game/src/Main/Model/Entity/Pet.java
+++ OOP Game/src/Main/Model/Entity/Pet.java
@@ -11,7 +11,7 @@ import java.awt.*;
 /**
  * Created by walkhard on 2/18/16.
  */
-public class Pet extends Entity {
+public class Pet extends Npc {
     public Pet(EntityTypeEnum entityType, EntitySpeechEnum entitySpiel, Occupation occupation, MapLocationPoint location){
         super(entityType, entitySpiel, occupation, location);
     }
